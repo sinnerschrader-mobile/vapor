d713fd0d4306c7a497de26a8e8c2670ca1fb8267
Nils Grabenhorst <nils.grabenhorst@sinnerschrader.com>
2020-11-03 11:50:56 +0100

    Restrict iOS builds to v13+
    Restricting iOS to version 13 and newer resolves dependency conflicts because some dependencies require iOS 13.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "vapor",
     platforms: [
-       .macOS(.v10_15)
+        .iOS(.v13),
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "Vapor", targets: ["Vapor"]),
